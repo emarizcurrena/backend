760f433d6349a7f1c348d846c14416a5d72d7ad7
emarizcurrena <emarizcurrena@gmail.com>
2023-08-08 22:54:16 -0300

    agregado de handlebars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import express from 'express'
+import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
 import productsRouter from './routes/products.router.js'
 import cartRouter from './routes/cart.router.js'
+import viewsRouter from './routes/views.router.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
 
+// HANDLEBARS
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+// ROUTES
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartRouter)
+app.use('/api/views', viewsRouter)
 
 
 const PORT = 8080
